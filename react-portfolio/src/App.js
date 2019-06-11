1e498c1d533eb512c270df4ca75e0abf977b2401
Chris Warner <christopher.warner92@gmail.com>
2019-06-11 19:00:29 -0400

    initial commit

---
+++ react-portfolio/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Jumbotron,Navbar,NavbarBrand} from 'react-bootstrap';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <NavbarBrand></NavbarBrand>
+      </header>
+    </div>
+  );
+}
+
+export default App;
