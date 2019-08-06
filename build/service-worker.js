78e9f8cc002ca60566514952bc1a516ab593683b
Christopher Warner <chriswarnertech@gmail.com>
2019-08-06 14:43:59 -0400

    build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3058d84968dd2e93e9bb9663867f18ff.js"
+  "/precache-manifest.652988324e8af9af824a4d495b6e91c8.js"
 );
 
 self.addEventListener('message', (event) => {
