33a0da1d0c5698a2b01a46a62ea5a8f9f5c14aed
Christopher Warner <chriswarnertech@gmail.com>
2019-08-06 16:09:56 -0400

    fixed router

--- build/precache-manifest.6f819bf5403e219057ce3cc8f0cff8b1.js
+++ build/precache-manifest.6f819bf5403e219057ce3cc8f0cff8b1.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0271ce556956795ca3142a8f736975fb",
+    "revision": "df5b835047c4241eaacd6122259d84f7",
     "url": "/react-portfolio/index.html"
   },
   {
-    "revision": "5c59142125cdb8b42001",
+    "revision": "026d53971bc1254cfd25",
     "url": "/react-portfolio/static/css/2.d819a5bb.chunk.css"
   },
   {
-    "revision": "b1a38d96229893122395",
+    "revision": "b91177e5a1f313796374",
     "url": "/react-portfolio/static/css/main.2cce8147.chunk.css"
   },
   {
-    "revision": "5c59142125cdb8b42001",
-    "url": "/react-portfolio/static/js/2.bcc3e07f.chunk.js"
+    "revision": "026d53971bc1254cfd25",
+    "url": "/react-portfolio/static/js/2.27a3dbbd.chunk.js"
   },
   {
-    "revision": "b1a38d96229893122395",
-    "url": "/react-portfolio/static/js/main.3376685f.chunk.js"
+    "revision": "b91177e5a1f313796374",
+    "url": "/react-portfolio/static/js/main.7de24ac4.chunk.js"
   },
   {
     "revision": "94872cf7e81768d148a3",
