0e1520fb04994b2ef560602de1af4bb9a17e4759
Christopher Warner <christopher.warner92@gmail.com>
2020-02-03 14:02:08 -0500

    Latest Build

---
+++ build/precache-manifest.94a751454d4335b3e04bac7611f234e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52d0d625dbf1f943adec3ced03e164ac",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "226e098c983c63fafd98",
+    "url": "/React-Portfolio/static/css/main.c4d60083.chunk.css"
+  },
+  {
+    "revision": "d66724baec9ada5e4be9",
+    "url": "/React-Portfolio/static/js/2.54388554.chunk.js"
+  },
+  {
+    "revision": "226e098c983c63fafd98",
+    "url": "/React-Portfolio/static/js/main.843acd07.chunk.js"
+  },
+  {
+    "revision": "a5982163e5c3e3252977",
+    "url": "/React-Portfolio/static/js/runtime~main.982e8b01.js"
+  },
+  {
+    "revision": "0fc602fcd5be427daf6ec77e142d0560",
+    "url": "/React-Portfolio/static/media/me.0fc602fc.png"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-portfolio/precache-manifest.7a566148c69cc56f4f37fab437af2375.js"
+  "/React-Portfolio/precache-manifest.94a751454d4335b3e04bac7611f234e3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-Portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
