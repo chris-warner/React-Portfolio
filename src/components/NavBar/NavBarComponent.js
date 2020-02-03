ed42f4f109486af391b38030fbcadb4c92cac650
Christopher Warner <christopher.warner92@gmail.com>
2020-02-03 13:39:45 -0500

    Created view component toggle to view portfolio in a timeline based format or grid based format

--- src/components/NavBar/NavBarComponent.js
+++ src/components/NavBar/NavBarComponent.js
@@ -5,14 +5,14 @@ import './NavBarComponent.css';
 function NavBarComponent() {
   return (
     <div>
-      <Navbar style={navbarStyle} collapseOnSelect expand="lg" bg="#2c3e50" variant="light">
-        <Navbar.Brand style={navbarStyle} href="/react-portfolio/#/">React-Portfolio</Navbar.Brand>
-        <Navbar.Toggle style={navbarStyle} bg="#2c3e50" aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse style={navbarStyle} id="responsive-navbar-nav">
-          <Nav style={navbarStyle} className="mr-auto">
-            <Nav.Link  style={navbarStyle} href="/react-portfolio/#/">Home</Nav.Link>
-            <Nav.Link style={navbarStyle}  href="/react-portfolio/#/portfolio">Portfolio</Nav.Link>
-            <NavDropdown style={navbarStyle} color="white" bg="#2c3e50" title="Social" variant="light" id="basic-nav-dropdown-light">
+      <Navbar className="navbarStyle" collapseOnSelect expand="lg" bg="#2c3e50" variant="light">
+        <Navbar.Brand classNamelass="navbarStyle" href="/react-portfolio/#/">React-Portfolio</Navbar.Brand>
+        <Navbar.Toggle className="navbarStyle" bg="#2c3e50" aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse className="navbarStyle" id="responsive-navbar-nav">
+          <Nav className="navbarStyle" className="mr-auto">
+            <Nav.Link className="navbarStyle" href="/react-portfolio/#/">Home</Nav.Link>
+            <Nav.Link className="navbarStyle"  href="/react-portfolio/#/portfolio">Portfolio</Nav.Link>
+            <NavDropdown className="navbarStyle" color="white" bg="#2c3e50" title="Social" variant="light" id="basic-nav-dropdown-light">
               <NavDropdown.Item href="https://github.com/chris-warner/">GitHub</NavDropdown.Item>
               <NavDropdown.Item href="https://www.linkedin.com/in/chris-warner-developer/">LinkedIn</NavDropdown.Item>
               <NavDropdown.Item href="chriswarnertech@gmail.com">Email</NavDropdown.Item>
@@ -23,13 +23,5 @@ function NavBarComponent() {
     </div>
   );
 }
-
-const navbarStyle = {
- color:"white",
- bg:"#4285f4",
- backgroundColor:"#4285f4"
-}
-
-
 
 export default NavBarComponent;
--- src/components/Pages/Portfolio/portfolio.js
+++ src/components/Pages/Portfolio/portfolio.js
@@ -2,12 +2,14 @@ import React from 'react';
 import NavBarComponent from '../../NavBar/NavBarComponent';
 import VerticleTimeline from '../../VerticleTimeline/VerticleTimeline';
 import FooterComponent from '../../Footer/FooterComponent';
+import ViewToggle from '../../ViewToggle/ViewToggleComponent';
 
 function Portfolio() {
     return (     
     <div>
       <NavBarComponent />
       <center><h1 style={titleStyle}>Projects and development.</h1></center>
+       <ViewToggle />
       <VerticleTimeline />
       <FooterComponent />
     </div>
