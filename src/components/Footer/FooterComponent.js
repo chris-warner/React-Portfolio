70094983121679e654ca1180cfb96f2d492e2526
Christopher Warner <christopher.warner92@gmail.com>
2020-02-06 13:44:09 -0500

    In this update, I changed the footer component to use react-bootstraps Navbar component so I can easily fix its position and keep it at the bottom of the screen

--- src/components/Footer/FooterComponent.js
+++ src/components/Footer/FooterComponent.js
@@ -1,17 +1,11 @@
 import React from 'react';
+import {Navbar} from 'react-bootstrap';
 import './FooterComponent.css';
 
 function FooterComponent() {
 
 return (
-  <footer id="sticky-footer" class=" footer py-4 bg-dark text-white-50">
-    <div class="container text-center">
-      {/* <small>Follow me on: </small> */}
-     <small> <a href="github.com/chris-warner">GitHub </a>
-      <a href="https://www.linkedin.com/in/chris-warner-developer/">LinkedIn</a></small>
-
-    </div>
-  </footer>
+<Navbar className="footer" fixed="bottom" />
 );
 }
 
