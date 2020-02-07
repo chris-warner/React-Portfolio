c4fae1e68f18d76d736409f39646b4ce6c6bebd8
Christopher Warner <christopher.warner92@gmail.com>
2020-02-07 15:06:07 -0500

    Updates

--- precache-manifest.2918d6296b963d0f31d3b1436d4d0c7f.js
+++ precache-manifest.2918d6296b963d0f31d3b1436d4d0c7f.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "771565430818e232a336673be89c4669",
+    "revision": "195b2414a93ae46404c6651fbbf20f1e",
     "url": "/React-Portfolio/index.html"
   },
   {
-    "revision": "fac1222c4c1eeef87c09",
+    "revision": "4c11eb4721a1473e78fc",
     "url": "/React-Portfolio/static/css/main.8f3ba3e7.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/React-Portfolio/static/js/2.6a732f64.chunk.js"
   },
   {
-    "revision": "fac1222c4c1eeef87c09",
-    "url": "/React-Portfolio/static/js/main.cfe1069b.chunk.js"
+    "revision": "4c11eb4721a1473e78fc",
+    "url": "/React-Portfolio/static/js/main.fa1551d3.chunk.js"
   },
   {
     "revision": "a5982163e5c3e3252977",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/React-Portfolio/precache-manifest.7ffae1ffbadc6fa5afde95860443d5c2.js"
+  "/React-Portfolio/precache-manifest.2918d6296b963d0f31d3b1436d4d0c7f.js"
 );
 
 self.addEventListener('message', (event) => {
