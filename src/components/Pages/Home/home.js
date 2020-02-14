2cda4c365bf5232f29b8e710e291c7d7e0ef1a44
Christopher Warner <christopher.warner92@gmail.com>
2020-02-13 20:31:46 -0500

    Imported and rendered all project icons in portfolio.

--- src/components/Pages/Home/home.js
+++ src/components/Pages/Home/home.js
@@ -5,39 +5,43 @@ import me from '../../../assets/img/me.png';
 import './home.css';
 
 function Home() {
-  return (
-    <div>
-      <NavBarComponent />
-      <div class="container d-flex align-items-center flex-column">
-        <br></br>
-        <img class="masthead-avatar mb-5" src={me} alt="" />
-        <h1 class="masthead-heading text-uppercase mb-0">Chris Warner</h1>
-        <div class="divider-custom divider-light">
-          <div class="divider-custom-line"></div>
-          <div class="divider-custom-icon">
-            <i class="fas fa-star"></i>
-          </div>
-          <div class="divider-custom-line"></div>
+    return (
+        <div>
+            <NavBarComponent/>
+            <div class="container d-flex align-items-center flex-column">
+                <br></br>
+                <img class="masthead-avatar mb-5"
+                    src={me}
+                    alt=""/>
+                <h1 class="masthead-heading text-uppercase mb-0">Chris Warner</h1>
+                <div class="divider-custom divider-light">
+                    <div class="divider-custom-line"></div>
+                    <div class="divider-custom-icon">
+                        <i class="fas fa-star"></i>
+                    </div>
+                    <div class="divider-custom-line"></div>
+                </div>
+                <p class="masthead-subheading font-weight-light mb-0">Chris Warner - Web Designer - Full Stack Development</p>
+                <p style={centerText}>
+                    My name is Chris Warner. I am a Full Stack Developer as well and a graduate of Columbia University. I have a strong commitment to teamwork and highly motivated to help others succeed. I started programming c# in the 7th grade and worked in the computer repair industry. I am also a musician and have worked on ambitious music productions and indie films. I am Highly creative in regards to problem-solving and innovation development, and I have taken my creativity and applied it to my programming.
+                        My skills include HTML5, CSS, M.E.R.N development stack, MySQL, MongoDB, React, JavaScript, HandleBars, MVC, as well as the ability to learn new languages quickly. During my studies, I have developed an app that generates a personalized investing strategy (mindstock) and another that allows you find top notch restaurants (ZipCrunch). For my final project I developed a platform that allows you to rent dogs from local shelter’s.</p>
+                <br></br>
+                <br></br>
+                <br></br>
+                {/* <Button style={btnStyle}>Portfolio</Button> */} </div>
+            <FooterComponent/>
         </div>
 
-        <p class="masthead-subheading font-weight-light mb-0">Chris Warner - Web Designer - Full Stack Development</p>
-        <p style={centerText}> My name is Chris Warner. I am a Full Stack Developer as well and a graduate of Columbia University. I have a strong commitment to teamwork and highly motivated to help others succeed. I started programming c# in the 7th grade and worked in the computer repair industry. I am also a musician and have worked on ambitious music productions and indie films. I am Highly creative in regards to problem-solving and innovation development, and I have taken my creativity and applied it to my programming.
-    My skills include HTML5, CSS, M.E.R.N development stack, MySQL, MongoDB, React, JavaScript, HandleBars, MVC, as well as the ability to learn new languages quickly. During my studies, I have developed an app that generates a personalized investing strategy (mindstock) and another that allows you find top notch restaurants (ZipCrunch). For my final project I developed a platform that allows you to rent dogs from local shelter’s.</p><br></br><br></br><br></br>
-        {/* <Button style={btnStyle}>Portfolio</Button> */}
-      </div>
-      <FooterComponent />
-    </div>
-
-  );
+    );
 }
 
 const centerText = {
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
-  marginTop: '50px',
-  textAlign: 'center',
-  color: 'black'
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: '50px',
+    textAlign: 'center',
+    color: 'black'
 }
 
 export default Home;
--- src/components/VerticleTimeline/VerticleTimeline.js
+++ src/components/VerticleTimeline/VerticleTimeline.js
@@ -7,6 +7,17 @@ import clickyGameImage from '../../assets/img/clicky-game.jpg';
 import tipCalculatorImage from '../../assets/img/tip.jpeg';
 import mindStockImage from '../../assets/img/MindStock.png';
 import friendFinderImage from '../../assets/img/FriendFinder.png';
+import zipCrunchImage from '../../assets/img/zipcrunch.png';
+import buddyImage from '../../assets/img/buddy.png';
+import fugatchImage from '../../assets/img/fugatch.jpeg';
+import burgerImage from '../../assets/img/burger.png';
+import bamazonImage from'../../assets/img/List Products.png';
+import liriImage from'../../assets/img/liri.png';
+import psychicGameImage from'../../assets/img/technics-q-c-640-480-1.jpg';
+import crystalGameImage from'../../assets/img/technics-q-c-640-480-2.jpg';
+import triviaGameImage from'../../assets/img/technics-q-c-640-480-5.jpg';
+import gifTasticImage from'../../assets/img/technics-q-c-640-480-7.jpg';
+import newYorkTimesSearchImage from'../../assets/img/technics-q-c-640-480-7.jpg';
 
 
 
@@ -17,7 +28,9 @@ function VerticleTimeline() {
         <VerticalTimelineElement
           className="vertical-timeline-element--work"
           date="July 2019"
-          iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}>
+          iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+          icon={<img className="contentImage" src={fugatchImage} />}
+          >
           <h3 className="vertical-timeline-element-title">Fugatch</h3>
           <h4 className="vertical-timeline-element-subtitle"> Technologies used in this project: HTML, Bootstrap.</h4>
           <p> Fugatch is a simple HTML website created for my client to post videos about his watch collection.</p>
@@ -29,7 +42,9 @@ function VerticleTimeline() {
         <VerticalTimelineElement
           className="vertical-timeline-element--work"
           date="July 2019"
-          iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}>
+          iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+          icon={<img className="contentImage" src={buddyImage} />}
+          >
           <h3 className="vertical-timeline-element-title">Buddy</h3>
           <h4 className="vertical-timeline-element-subtitle"> Technologies used in this project: HTML, Bootstrap, React, Express, Sequelize, MySQL, Firebase.</h4>
           <p>Buddy is a full stack MERN application created that allows people to rent dogs from local shelters.</p>
@@ -42,7 +57,7 @@ function VerticleTimeline() {
           date="June 2019"
           iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
           icon={<img className="contentImage" src={tipCalculatorImage} />}
-          >
+        >
           <h3 className="vertical-timeline-element-title">React Tip Calculator</h3>
           <h4 className="vertical-timeline-element-subtitle"> Technologies used in this project: React and Javascript.</h4>
           <p>Tip calculator is a simple application that allow you to calculate a tip for your server. One of it's key features is the ability to divide the tip between multiple people. Simply fill out the form and click calculate and see the results.</p>
@@ -55,7 +70,7 @@ function VerticleTimeline() {
           date="May - June 2019"
           iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
           icon={<img className="contentImage" src={mindStockImage} />}
-          >
+        >
           <h3 className="vertical-timeline-element-title">MindStock</h3>
           <h4 className="vertical-timeline-element-subtitle"> Technologies used in this project: : HTML, CSS, Node.js, Express, handlebars, Sequelize, Heroku, JawsDB, MySQL. .</h4>
           <p>MindStock uses a personality assessment to figure out risk tolerance to help investors pick the right fund for them. It provides a starter guide through what a mutual fund is and what information is relevant to navigate the investment universe.</p>
@@ -64,12 +79,12 @@ function VerticleTimeline() {
 
         <VerticalTimelineElement
           className="vertical-timeline-element--work"
-          date="May - June 2019" 
+          date="May - June 2019"
           iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
           layout={'3-columns'}
           icon={<img className="contentImage" src={clickyGameImage} />}>
           <h3 className="vertical-timeline-element-title">Clicky Game</h3>
-            <h4 className="vertical-timeline-element-subtitle"> Technologies used in this project: React.</h4>
+          <h4 className="vertical-timeline-element-subtitle"> Technologies used in this project: React.</h4>
           <p>Test your memory with Clicky Game. Click an image to begin, try not click the same image twice. Technologies used in this project: React, React-Bootstrap.
 </p>
           <div> <a href="https://github.com/chris-warner/clicky-game">GitHub </a><a href="https://chris-warner.github.io/clicky-game/">Demo</a></div>
@@ -78,7 +93,9 @@ function VerticleTimeline() {
         <VerticalTimelineElement
           className="vertical-timeline-element--work"
           date="April - May 2019"
-          iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}>
+          iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+          icon={<img className="contentImage" src={burgerImage} />}
+          >
           <h3 className="vertical-timeline-element-title">Eat Da Burger</h3>
           <h4 className="vertical-timeline-element-subtitle"> Technologies used in this project:  HTML, CSS, Node.js, Express, Handlebars, Sequelize, Heroku, JawsDB, MySQL.</h4>
           <p>Eat Da Burger is a node applicastion that utilized express, express-handlebars and mysql. Enter a burger in the textbox and hit submit to add the burger to the queue. This adds a burger to the table in mysql. Hit Devour It to "devour" the burger, and check "devoured" in said database to true.</p>
@@ -89,7 +106,7 @@ function VerticleTimeline() {
           className="vertical-timeline-element--work"
           date="May 2019"
           iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-          icon={<img className="contentImage" src={friendFinderImage} />}>
+          icon={<img className="contentImage" src={friendFinderImage} />}
           >
           <h3 className="vertical-timeline-element-title">FriendFinder</h3>
           <h4 className="vertical-timeline-element-subtitle"> Technologies used in this project: HTML, CSS, Node.js, Express, Sequelize, Heroku.</h4>
@@ -100,7 +117,9 @@ function VerticleTimeline() {
         <VerticalTimelineElement
           className="vertical-timeline-element--work"
           date="May 2019"
-          iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}>
+          iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+          icon={<img className="contentImage" src={bamazonImage} />}
+          >
           <h3 className="vertical-timeline-element-title">bamazon</h3>
           <h4 className="vertical-timeline-element-subtitle"> Technologies used in this project: Javascript, Node.js, MySQL and express. Bamazon is an Amazon-like storefront utilizing Node and MySQL. </h4>
           <p>To use it, type node bamazonCustomer.js in the terminal. You will be presented with a database of products. Enter the product ID you would like to to purchase and quantity of items in the following terminal prompt. If there are enough items in stock, the purchase will be successful and you will be presented with the order total. The stock_quantity field will then be updated in mysql to reflect the current value. This is a program that runs in the terminal so there is no live demo.</p>
@@ -110,7 +129,8 @@ function VerticleTimeline() {
         <VerticalTimelineElement
           className="vertical-timeline-element--work"
           date="March - May 2019"
-          iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}>
+          iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+          icon={<img className="contentImage" src={liriImage} />}>
           <h3 className="vertical-timeline-element-title">Liri Node App</h3>
           <h4 className="vertical-timeline-element-subtitle"> Javascript, Node.js, axios, dotenv, Spotify API, Bands in Town API. </h4>
           <p>Use Liri to find the latest Concerts, Bands, and songs you love, from your console. Using Spotify, Bandsintown and omdb, Liri can retrieve concert dates for your favorite bands, movie information for your favorite films, song information for you favorite songs. This is a program that runs in the terminal so there is no live demo.</p>
@@ -120,7 +140,8 @@ function VerticleTimeline() {
         <VerticalTimelineElement
           className="vertical-timeline-element--work"
           date="May 2019"
-          iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}>
+          iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+          icon={<img className="contentImage" src={psychicGameImage} />}>
           <h3 className="vertical-timeline-element-title">Psychic Game</h3>
           <h4 className="vertical-timeline-element-subtitle"> Technologies used in this project: HTML, CSS, javascript and JQuery.</h4>
           <p>Try and guess what letter the computer picks by pressing keys. If you guess right, you win a point. If you fail to guess correctly 10 times you loose a point.</p>
@@ -130,7 +151,9 @@ function VerticleTimeline() {
         <VerticalTimelineElement
           className="vertical-timeline-element--work"
           date="May 2019"
-          iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}>
+          iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+          icon={<img className="contentImage" src={crystalGameImage} />}
+          >
           <h3 className="vertical-timeline-element-title">Crystal Game</h3>
           <h4 className="vertical-timeline-element-subtitle"> Technologies used in this project: HTML, CSS, javascript and JQuery.</h4>
           <p>You will be given a random number at the start of the game. There are four crystals below. By clicking on a crystal you will add a specific amount of points to your total score. You win the game by matching your total score to the random number, you lose if the total score goes above the random number. The value of each crystal is hidden from you until you click on it. Each time when the game starts, the game will change the values of each crystal. </p>
@@ -141,6 +164,8 @@ function VerticleTimeline() {
           className="vertical-timeline-element--work"
           date="May 2019"
           iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+          icon={<img className="contentImage" src={triviaGameImage} />}
+
         >
           <h3 className="vertical-timeline-element-title">Trivia Game</h3>
           <h4 className="vertical-timeline-element-subtitle"> Technologies used in this project: HTML, CSS, javascript and JQuery.</h4>
@@ -152,6 +177,7 @@ function VerticleTimeline() {
           className="vertical-timeline-element--work"
           date="May 2019"
           iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+          icon={<img className="contentImage" src={gifTasticImage} />}
         >
           <h3 className="vertical-timeline-element-title">GifTastic</h3>
           <h4 className="vertical-timeline-element-subtitle"> Technologies used in this project: HTML, CSS, Giphy API, javascript and JQuery.</h4>
@@ -163,6 +189,7 @@ function VerticleTimeline() {
           className="vertical-timeline-element--work"
           date="May 2019"
           iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+          icon={<img className="contentImage" src={newYorkTimesSearchImage} />}
         >
           <h3 className="vertical-timeline-element-title">New York Times Search</h3>
           <h4 className="vertical-timeline-element-subtitle"> Technologies used in this project: HTML, CSS, Yelp API, New York City Department of Health API, Google Maps API, javascript and JQuery.</h4>
@@ -177,6 +204,8 @@ function VerticleTimeline() {
           className="vertical-timeline-element--work"
           date="Feburary 2019"
           iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+          icon={<img className="contentImage" src={zipCrunchImage} />}
+
         >
           <h3 className="vertical-timeline-element-title">ZipCrunch</h3>
           <h4 className="vertical-timeline-element-subtitle">Technologies used in this project: html, css, Yelp API, New York City Department of Health API, Google Maps API, javascript and JQuery.</h4>
