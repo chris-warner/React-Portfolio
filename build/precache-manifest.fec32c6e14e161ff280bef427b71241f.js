7205fb184d7da94d83202d8f1c01ed728d7cee35
Christopher Warner <christopher.warner92@gmail.com>
2020-02-20 13:56:30 -0500

    Upgraded packages.

---
+++ build/precache-manifest.fec32c6e14e161ff280bef427b71241f.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2accd5cd008c83b48ec4955fe4126987",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9669ac2a43a7cc6bcc1f",
+    "url": "/static/css/main.b76454b1.chunk.css"
+  },
+  {
+    "revision": "2699865c4c05190be137",
+    "url": "/static/js/2.9c2f61b1.chunk.js"
+  },
+  {
+    "revision": "9669ac2a43a7cc6bcc1f",
+    "url": "/static/js/main.72cb747b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "267092da2b5b983757e9909aa706f1cd",
+    "url": "/static/media/FriendFinder.267092da.png"
+  },
+  {
+    "revision": "e223f027782b6d9119d30a51c420185d",
+    "url": "/static/media/List Products.e223f027.png"
+  },
+  {
+    "revision": "32b9bbc283e8d8b8d2d33ea680dca61d",
+    "url": "/static/media/MindStock.32b9bbc2.png"
+  },
+  {
+    "revision": "b2a2afebeb8d171b3c26d5ca6b23b6a5",
+    "url": "/static/media/buddy.b2a2afeb.png"
+  },
+  {
+    "revision": "9cc268cce2f0f20798480e94818c3a1a",
+    "url": "/static/media/burger.9cc268cc.png"
+  },
+  {
+    "revision": "6b170b4f4d2fb1cbea7fd8ac4ba977ff",
+    "url": "/static/media/clicky-game.6b170b4f.jpg"
+  },
+  {
+    "revision": "329b593c0058db4509b677390698ed36",
+    "url": "/static/media/fugatch.329b593c.jpeg"
+  },
+  {
+    "revision": "ab1b481350e4e0cc0bfb56901065832f",
+    "url": "/static/media/liri.ab1b4813.png"
+  },
+  {
+    "revision": "0fc602fcd5be427daf6ec77e142d0560",
+    "url": "/static/media/me.0fc602fc.png"
+  },
+  {
+    "revision": "1145b07370bfce33220feb56db738837",
+    "url": "/static/media/technics-q-c-640-480-1.1145b073.jpg"
+  },
+  {
+    "revision": "ec0dbfa8bf1f1dffd3cdac76b361b030",
+    "url": "/static/media/technics-q-c-640-480-2.ec0dbfa8.jpg"
+  },
+  {
+    "revision": "9dffb04b8666f2a2f9c7a80d65aeed1b",
+    "url": "/static/media/technics-q-c-640-480-5.9dffb04b.jpg"
+  },
+  {
+    "revision": "e57d9f786a84773e8f830e03e8966bc4",
+    "url": "/static/media/technics-q-c-640-480-7.e57d9f78.jpg"
+  },
+  {
+    "revision": "8cf47f0020d5ca9aab00bd1407743978",
+    "url": "/static/media/tip.8cf47f00.jpeg"
+  },
+  {
+    "revision": "e6c412872603f08788102c5f13c51855",
+    "url": "/static/media/zipcrunch.e6c41287.png"
+  }
+]);
