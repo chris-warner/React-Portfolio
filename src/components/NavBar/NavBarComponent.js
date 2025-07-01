a86c039f618729c10da5f628959269edf4647240
codeCrafter247 <codeCrafter247@gmail.com>
2025-06-30 21:24:25 -0400

    style: Update Footer and NavBar styles; enhance Home component layout

--- src/components/NavBar/NavBarComponent.js
+++ src/components/NavBar/NavBarComponent.js
@@ -6,17 +6,24 @@ import './NavBarComponent.css';
 function NavBarComponent() {
   return (
     <div>
-      <Navbar className="navbarStyle" collapseOnSelect bg="#2c3e50" variant="light">
-        <Navbar.Brand className="navbarStyle" as={Link} to="/">React-Portfolio</Navbar.Brand> {/* Use Link component for internal navigation */}
-        <Navbar.Toggle className="navbarStyle" bg="#2c3e50" aria-controls="responsive-navbar-nav"/>
+      <Navbar className="navbarStyle" expand="md" variant="light" sticky="top">
+        <Navbar.Brand as={Link} to="/">React-Portfolio</Navbar.Brand> {/* Use Link component for internal navigation */}
+        <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
         <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="navbarStyle">
-            <Nav.Link className="navbarStyle" as={Link} to="/">Home</Nav.Link> {/* Use Link component for internal navigation */}
-            <Nav.Link className="navbarStyle" as={Link} to="/portfolio">Portfolio</Nav.Link> {/* Use Link component for internal navigation */}
-            <Nav.Link className="navbarStyle" href="/Christopher_Warner_Resume_2024.pdf" download>
+          <Nav className="ml-auto">
+            <Nav.Link as={Link} to="/">Home</Nav.Link> {/* Use Link component for internal navigation */}
+            <Nav.Link as={Link} to="/portfolio">Portfolio</Nav.Link> {/* Use Link component for internal navigation */}
+            <Nav.Link href="/Christopher_Warner_Resume_2024.pdf" download>
               Resume
             </Nav.Link>
-            <NavDropdown className="navbarStyle" color="white" bg="#2c3e50" title="Social" variant="light">
+            <NavDropdown
+              title="Social"
+              id="collasible-nav-dropdown"
+              align="end"
+              popperConfig={{
+                strategy: "fixed"
+              }}
+            >
               <NavDropdown.Item href="https://github.com/chris-warner/" target="_blank" rel="noopener noreferrer">GitHub</NavDropdown.Item>
               <NavDropdown.Item href="https://www.linkedin.com/in/chris-warner-developer/" target="_blank" rel="noopener noreferrer">LinkedIn</NavDropdown.Item>
               <NavDropdown.Item href="mailto:chriswarnertech@gmail.com">Email</NavDropdown.Item>
--- src/components/Pages/Home/home.js
+++ src/components/Pages/Home/home.js
@@ -9,29 +9,17 @@ function Home() {
         <div>
             <NavBarComponent/>
             <div className="container d-flex align-items-center flex-column">
-                <br></br>
-                <img className="masthead-avatar mb-5"
-                    src={me}
-                    alt=""/>
-                <h1 className="masthead-heading text-uppercase mb-0">Chris Warner</h1>
-                <div className="divider-custom divider-light">
-                    <div className="divider-custom-line"></div>
-                    <div className="divider-custom-icon">
-                        <i className="fas fa-star"></i>
+                <div className="home-hero">
+                    <img src={me} alt="Chris Warner" className="home-avatar" />
+                    <div className="home-title">CHRIS WARNER</div>
+                    <div className="home-subtitle">Web Designer &ndash; Full Stack Development</div>
+                    <div className="home-description">
+                        My name is Chris Warner. I am a Full Stack Developer as well as a graduate of Columbia University. I have a strong commitment to teamwork and highly motivated to help others succeed. I started programming C# in the 7th grade and worked in the computer repair industry. I am also a musician and have worked on ambitious music productions and indie films. I am highly creative in regards to problem-solving and innovation development, and I have taken my creativity and applied it to my programming. My skills include HTML5, CSS, M.E.R.N development stack, MySQL, MongoDB, React, JavaScript, HandleBars, MVC, as well as the ability to learn new languages quickly. During my studies, I have developed an app that generates a personalized investing strategy (Mindstock) and another that allows you to find top notch restaurants (ZipCrunch). For my final project I developed a platform that allows you to rent dogs from local shelters.
                     </div>
-                    <div className="divider-custom-line"></div>
                 </div>
-                <p className="masthead-subheading font-weight-light mb-0">Chris Warner - Web Designer - Full Stack Development</p>
-                <p style={centerText}>
-                    My name is Chris Warner. I am a Full Stack Developer as well and a graduate of Columbia University. I have a strong commitment to teamwork and highly motivated to help others succeed. I started programming c# in the 7th grade and worked in the computer repair industry. I am also a musician and have worked on ambitious music productions and indie films. I am Highly creative in regards to problem-solving and innovation development, and I have taken my creativity and applied it to my programming.
-                        My skills include HTML5, CSS, M.E.R.N development stack, MySQL, MongoDB, React, JavaScript, HandleBars, MVC, as well as the ability to learn new languages quickly. During my studies, I have developed an app that generates a personalized investing strategy (mindstock) and another that allows you find top notch restaurants (ZipCrunch). For my final project I developed a platform that allows you to rent dogs from local shelter’s.</p>
-                <br></br>
-                <br></br>
-                <br></br>
-                {/* <Button style={btnStyle}>Portfolio</Button> */} </div>
+            </div>
             <FooterComponent/>
         </div>
-
     );
 }
 
